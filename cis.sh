db27edc7733ca73056b5d7c4267343f688c83802
BuildTools <unconfigured@null.spigotmc.org>
2019-11-01 00:33:28 +0000

    Update cis.sh

--- cis.sh
+++ cis.sh
@@ -13,40 +13,31 @@ SCRIPT_DIR="$(dirname "$0")"
 #Netowrk 
 echo_status "3.1.1 Ensure IP forwarding is disabled"
 echo "net.ipv4.ip_forward=0" | sudo tee -a /etc/sysctl.conf
-echo "net.ipv4.route.flush=1" | sudo tee -a /etc/sysctl.conf
 echo_status "3.1.2 Ensure packet redirect sending is disabled"
 echo "net.ipv4.conf.all.send_redirects=0" | sudo tee -a /etc/sysctl.conf
 echo "net.ipv4.conf.default.send_redirects=0" | sudo tee -a /etc/sysctl.conf
-echo "net.ipv4.route.flush=1" | sudo tee -a /etc/sysctl.conf
 echo_status "3.2.1 Ensure source routed packets are not accepted"
 echo "net.ipv4.conf.all.accept_source_route=0" | sudo tee -a /etc/sysctl.conf
 echo "net.ipv4.conf.default.accept_source_route=0" | sudo tee -a /etc/sysctl.conf
-echo "net.ipv4.route.flush=1" | sudo tee -a /etc/sysctl.conf
 echo_status "3.2.2 Ensure ICMP redirects are not accepted"
 echo "net.ipv4.conf.all.accept_redirects=0" | sudo tee -a /etc/sysctl.conf
 echo "net.ipv4.conf.default.accept_redirects=0" | sudo tee -a /etc/sysctl.conf
-echo "net.ipv4.route.flush=1" | sudo tee -a /etc/sysctl.conf
 echo_status "3.2.3 Ensure secure ICMP redirects are not accepted"
 echo "net.ipv4.conf.all.secure_redirects=0" | sudo tee -a /etc/sysctl.conf
 echo "net.ipv4.conf.default.secure_redirects=0" | sudo tee -a /etc/sysctl.conf
-echo "net.ipv4.route.flush=1" | sudo tee -a /etc/sysctl.conf
 echo_status "3.2.4 Ensure suspicious packets are logged"
 echo "net.ipv4.conf.all.log_martians=1" | sudo tee -a /etc/sysctl.conf
 echo "net.ipv4.conf.default.log_martians=1" | sudo tee -a /etc/sysctl.conf
-echo "net.ipv4.route.flush=1" | sudo tee -a /etc/sysctl.conf
 echo_status "3.2.5 Ensure broadcast ICMP requests are ignored"
 echo "net.ipv4.icmp_echo_ignore_broadcasts=1" | sudo tee -a /etc/sysctl.conf
-echo "net.ipv4.route.flush=1" | sudo tee -a /etc/sysctl.conf
 echo_status "3.2.6 Ensure bogus ICMP responses are ignored"
 echo "net.ipv4.icmp_ignore_bogus_error_responses=1" | sudo tee -a /etc/sysctl.conf
-echo "net.ipv4.route.flush=1" | sudo tee -a /etc/sysctl.conf
 echo_status "3.2.7 Ensure Reverse Path Filtering is enabled"
 echo "net.ipv4.conf.all.rp_filter=1" | sudo tee -a /etc/sysctl.conf
 echo "net.ipv4.conf.default.rp_filter=1" | sudo tee -a /etc/sysctl.conf
-echo "net.ipv4.route.flush=1" | sudo tee -a /etc/sysctl.conf
 echo_status "3.2.8 Ensure TCP SYN Cookies is enabled"
 echo "net.ipv4.tcp_syncookies=1" | sudo tee -a /etc/sysctl.conf
-echo "net.ipv4.route.flush=1" | sudo tee -a /etc/sysctl.conf
+echo_status "Disable IPv6"
 echo "net.ipv6.conf.all.accept_ra=0" | sudo tee -a /etc/sysctl.conf
 echo "net.ipv6.conf.default.accept_ra=0" | sudo tee -a /etc/sysctl.conf
 echo "net.ipv6.route.flush=1" | sudo tee -a /etc/sysctl.conf
@@ -161,9 +152,7 @@ else
     if [ `echo $fileperm | cut -c9` != "-" ]; then
      echo "Other Write permission set on file $file"
     fi
-    if [ ! -h "$file" -a -f "$file" ]; then
-     echo ".rhosts file in $dir"
-    fi
+   fi
   done
   if [ ! -h "$dir/.forward" -a -f "$dir/.forward" ]; then
    echo ".forward file $dir/.forward exists"
@@ -171,6 +160,11 @@ else
   if [ ! -h "$dir/.netrc" -a -f "$dir/.netrc" ]; then
    echo ".netrc file $dir/.netrc exists"
   fi
+  for file in $dir/.rhosts; do
+   if [ ! -h "$file" -a -f "$file" ]; then
+    echo ".rhosts file in $dir"
+   fi
+  done
 fi
 done
 pause "[PAUSED] Fix any of the problems listed"
