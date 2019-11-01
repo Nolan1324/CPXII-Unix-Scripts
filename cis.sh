ca60233dfbcefad9fe840b9be04b752201e3ba7c
Nolan Kuza <nkuza8@gmail.com>
2019-10-31 20:18:35 -0400

    Update cis.sh

--- cis.sh
+++ cis.sh
@@ -132,7 +132,7 @@ shift
 done
 pause "[PAUSED] Fix any issues listed"
 
-echo_status "6.2.7-9 Ensure all users' home directories exist, are permissions are 750 or more restrictive, and are owned by their own users"
+echo_status "6.2.7-12 User home directory auditing"
 cat /etc/passwd | egrep -v '^(root|halt|sync|shutdown)' | awk -F: '($7 != "/usr/sbin/nologin" && $7 != "/bin/false") { print $1 " " $6 }' | while read user dir; do
 if [ ! -d "$dir" ]; then
   echo "The home directory ($dir) of user $user does not exist."
@@ -164,10 +164,58 @@ else
      echo "Other Write permission set on file $file"
     fi
    done
+   if [ ! -h "$dir/.forward" -a -f "$dir/.forward" ]; then
+    echo ".forward file $dir/.forward exists"
+   fi
+   if [ ! -h "$dir/.netrc" -a -f "$dir/.netrc" ]; then
+    echo ".netrc file $dir/.netrc exists"
+   fi
+   if [ ! -h "$file" -a -f "$file" ]; then
+    echo ".rhosts file in $dir"
+   fi
 fi
 done
 pause "[PAUSED] Fix any of the problems listed"
-
+echo_status "6.2.15 Ensure all groups in /etc/passwd exist in /etc/group"
+for i in $(cut -s -d: -f4 /etc/passwd | sort -u ); do
+  grep -q -P "^.*?:[^:]*:$i:" /etc/group
+  if [ $? -ne 0 ]; then
+    echo "Group $i is referenced by /etc/passwd but does not exist in /etc/group"
+  fi
+done
+echo_status "6.2.16 Ensure no duplicate UIDs exist"
+cat /etc/passwd | cut -f3 -d":" | sort -n | uniq -c | while read x ; do [ -z "${x}" ] && break
+  set - $x
+  if [ $1 -gt 1 ]; then
+    users=`awk -F: '($3 == n) { print $1 }' n=$2 /etc/passwd | xargs`
+    echo "Duplicate UID ($2): ${users}"
+  fi
+done
+echo_status "6.2.17 Ensure no duplicate GIDs exist"
+cat /etc/group | cut -f3 -d":" | sort -n | uniq -c | while read x ; do [ -z "${x}" ] && break
+  set - $x
+  if [ $1 -gt 1 ]; then
+    groups=`awk -F: '($3 == n) { print $1 }' n=$2 /etc/group | xargs`
+  echo "Duplicate GID ($2): ${groups}"
+  fi
+done
+echo_status "6.2.18 Ensure no duplicate user names exist"
+cat /etc/passwd | cut -f1 -d":" | sort -n | uniq -c | while read x ; do [ -z "${x}" ] && break
+  set - $x
+  if [ $1 -gt 1 ]; then
+    uids=`awk -F: '($1 == n) { print $3 }' n=$2 /etc/passwd | xargs`
+    echo "Duplicate User Name ($2): ${uids}"
+  fi
+done
+echo_status "6.2.19 Ensure no duplicate group names exist"
+cat /etc/group | cut -f1 -d":" | sort -n | uniq -c | while read x ; do [ -z "${x}" ] && break
+  set - $x
+  if [ $1 -gt 1 ]; then
+    gids=`gawk -F: '($1 == n) { print $3 }' n=$2 /etc/group | xargs`
+    echo "Duplicate Group Name ($2): ${gids}"
+  fi
+done
+pause "[PAUSED] Fix any of the problems listed in 6.2.15-19"
 echo_status "6.2.20 Ensure shadow group is empty"
 grep ^shadow:[^:]*:[^:]*:[^:]+ /etc/group
 awk -F: '($4 == "<shadow-gid>") { print }' /etc/passwd
